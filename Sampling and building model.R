e69d0e94cfe5dacbfb575a2b49def7677b8e97f8
MeatP1E <phamkhoi282002@gmail.com>
2023-05-12 21:54:25 +1000

    change

--- Sampling and building model.R
+++ Sampling and building model.R
@@ -2,18 +2,18 @@ install.packages("rpart")
 install.packages("rpart.plot")
 
 library(rpart)
+library(rpart.plot)
 
+columns <- c("age","marital","education","housing", "y")
 
-columns <- c("age","new_marital","new_education","new_housing", "y")
-
-bank_clean<-banktest1[columns]
+bank_clean<-bank[columns]
 
 str(bank_clean)
 dim(bank_clean)
 summary(bank_clean)
 
 
-set.seed(555)#Why do you need to set seed?
+set.seed(777)#Why do you need to set seed?
 
 train.index <- sample(1:nrow(bank_clean), 0.7*nrow(bank_clean))
 bank_clean.train <- bank_clean[train.index,]
@@ -25,7 +25,7 @@ dim(bank_clean.test)
 # Default decision tree model
 # Builds a decision tree from the iris dataset to predict
 # species given all other columns as predictors
-bank_clean.tree <- rpart(y~.,data=bank_clean.train, method="class",control =rpart.control(minsplit =1,minbucket=7, cp=0))
+bank_clean.tree <- rpart(y~.,data=bank_clean.train, method="class",control =rpart.control(minsplit =1,minbucket=6, cp=0))
 
 # Reports the model
 print(bank_clean.tree)

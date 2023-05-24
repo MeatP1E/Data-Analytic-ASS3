d1279655b37a76694dc9fd5512e113597eaaa3a4
behan458 <132244617+behan458@users.noreply.github.com>
2023-05-25 01:00:53 +1000

    Updated

--- ANN Model.R
+++ ANN Model.R
@@ -25,47 +25,27 @@ bank_clean.train <- bank_clean[train.index, ]
 bank_clean.test <- bank_clean[-train.index, ]
 
 # Fit neural network
-nn <- neuralnet(new_education ~ ., data = bank_clean.train, hidden = 2, act.fct = "logistic", linear.output = FALSE)
+nn <- neuralnet(y~ ., data = bank_clean.train, hidden = 2, act.fct = "logistic", linear.output = FALSE)
 
 # Plot neural network
 plot(nn)
+# Generate predictions on the test dataset
+predictions <- predict(nn, bank_clean.test)
+predicted_classes <- ifelse(predictions > 0.5, 1, 0)
 
-# Make predictions on the test data
-nn.predict <- predict(nn, bank_clean.test[, -ncol(bank_clean.test)])
+# Create the confusion matrix
+confusion_matrix <- table(Actual = bank_clean.test$y, Predicted = predicted_classes)
 
-# Convert predictions to binary values
-predicted_classes <- ifelse(nn.predict > 0.5, 1, 0)
+# Create a caret confusion matrix object
+cm <- confusionMatrix(confusion_matrix)
 
-# Evaluate model performance
-confusion_matrix <- table(Actual = bank_clean.test$new_education, Predicted = predicted_classes)
-accuracy <- sum(diag(confusion_matrix)) / sum(confusion_matrix)
+# Plot the confusion matrix
+plot(cm$table, col = cm$byClass, 
+     main = paste("Confusion Matrix\nAccuracy:", cm$overall["Accuracy"]))
 
-# Check if positive class exists in the predictions
-if ("1" %in% colnames(confusion_matrix) && "1" %in% rownames(confusion_matrix)) {
-  precision <- confusion_matrix["1", "1"] / sum(confusion_matrix[, "1"])
-  recall <- confusion_matrix["1", "1"] / sum(confusion_matrix["1", ])
-  f1_score <- 2 * precision * recall / (precision + recall)
-} else {
-  precision <- 0
-  recall <- 0
-  f1_score <- 0
-}
-
-# Create a new confusion matrix with additional rows and columns
-new_confusion_matrix <- matrix(0, nrow = nrow(confusion_matrix) + 2, ncol = ncol(confusion_matrix) + 2)
-rownames(new_confusion_matrix) <- c(rownames(confusion_matrix), "TN", "FP")
-colnames(new_confusion_matrix) <- c(colnames(confusion_matrix), "FN", "TP")
-
-# Copy values from the original confusion matrix to the new matrix
-new_confusion_matrix[1:nrow(confusion_matrix), 1:ncol(confusion_matrix)] <- confusion_matrix
-
-# Plot the confusion matrix using heatmap.2
-heatmap.2(new_confusion_matrix, trace = "none", col = c("lightgray", "lightblue"),
-          main = "Confusion Matrix", xlab = "Predicted", ylab = "Actual",
-          margins = c(8, 8))
+# Add labels to the plot
+legend("bottomright", legend = c("0", "1"), 
+       fill = cm$byClass, title = "Class")
 
 # Print the evaluation metrics
-cat("\nAccuracy:", accuracy)
-cat("\nPrecision:", precision)
-cat("\nRecall:", recall)
-cat("\nF1-Score:", f1_score)
+print(cm)

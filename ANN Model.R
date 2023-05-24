590ec47a91b5f08c955fb9a4ff3f69f992b24ff0
behan458 <132244617+behan458@users.noreply.github.com>
2023-05-24 15:21:12 +1000

    with confusion matrix

--- ANN Model.R
+++ ANN Model.R
@@ -2,6 +2,7 @@ library(rpart)
 library(rpart.plot)
 library(caret)
 library(neuralnet)
+library(gplots)
 
 banktest1 <- read.csv("~/banktest1.csv")
 
@@ -28,3 +29,43 @@ nn <- neuralnet(new_education ~ ., data = bank_clean.train, hidden = 2, act.fct 
 
 # Plot neural network
 plot(nn)
+
+# Make predictions on the test data
+nn.predict <- predict(nn, bank_clean.test[, -ncol(bank_clean.test)])
+
+# Convert predictions to binary values
+predicted_classes <- ifelse(nn.predict > 0.5, 1, 0)
+
+# Evaluate model performance
+confusion_matrix <- table(Actual = bank_clean.test$new_education, Predicted = predicted_classes)
+accuracy <- sum(diag(confusion_matrix)) / sum(confusion_matrix)
+
+# Check if positive class exists in the predictions
+if ("1" %in% colnames(confusion_matrix) && "1" %in% rownames(confusion_matrix)) {
+  precision <- confusion_matrix["1", "1"] / sum(confusion_matrix[, "1"])
+  recall <- confusion_matrix["1", "1"] / sum(confusion_matrix["1", ])
+  f1_score <- 2 * precision * recall / (precision + recall)
+} else {
+  precision <- 0
+  recall <- 0
+  f1_score <- 0
+}
+
+# Create a new confusion matrix with additional rows and columns
+new_confusion_matrix <- matrix(0, nrow = nrow(confusion_matrix) + 2, ncol = ncol(confusion_matrix) + 2)
+rownames(new_confusion_matrix) <- c(rownames(confusion_matrix), "TN", "FP")
+colnames(new_confusion_matrix) <- c(colnames(confusion_matrix), "FN", "TP")
+
+# Copy values from the original confusion matrix to the new matrix
+new_confusion_matrix[1:nrow(confusion_matrix), 1:ncol(confusion_matrix)] <- confusion_matrix
+
+# Plot the confusion matrix using heatmap.2
+heatmap.2(new_confusion_matrix, trace = "none", col = c("lightgray", "lightblue"),
+          main = "Confusion Matrix", xlab = "Predicted", ylab = "Actual",
+          margins = c(8, 8))
+
+# Print the evaluation metrics
+cat("\nAccuracy:", accuracy)
+cat("\nPrecision:", precision)
+cat("\nRecall:", recall)
+cat("\nF1-Score:", f1_score)

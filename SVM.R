2742aa01c417b4f22870e5bd0a4cb743062a0880
MeatP1E <phamkhoi282002@gmail.com>
2023-05-25 11:54:07 +1000

    svm change

--- SVM.R
+++ SVM.R
@@ -2,40 +2,37 @@ library(e1071)
 library(caret)
 
 bank_clean1<-bank_clean
-bank_clean1$y <- factor(bank_clean1$y)
-bank_clean1$y <- ifelse(bank_clean1$y == "yes", 1, 0)
+bank_clean$y <- factor(bank_clean$y)
 
 set.seed(777)
 
-train.index <- sample(1:nrow(bank_clean1), 0.7*nrow(bank_clean1))
-bank_clean1.train <- bank_clean1[train.index,]
-dim(bank_clean1.train)
-## select the 30% left as the testing data
-bank_clean1.test <- bank_clean1[-train.index,]
-dim(bank_clean1.test)
-
 # Build the SVM model
-svm_model <- svm(y ~ ., data = bank_clean1.train, kernel = "radial")
+svm_model <- svm(y ~ ., data = bank_clean.train, kernel = "radial")
 formula<-y~age+marital+education+housing
 
 # Visualize the model using a scatterplot
-plot(svm_model, bank_clean1.train, formula)
+plot(svm_model, bank_clean.train, formula)
+model <- svm(y ~ ., data = bank_clean.train)
+plot(cmdscale(dist(bank_clean.train[,-5])),
+     col = as.integer(bank_clean.train[,5]),
+     pch = c("o","+")[1:150 %in% model$index + 1])
 
 
 # Make predictions on the test data
-bank_clean1.predictions <- predict(svm_model, bank_clean1.test, type = "class")
-head(bank_clean1.predictions)
+bank_clean.predictions <- predict(svm_model, bank_clean.test, type = "class")
+head(bank_clean.predictions)
 
 
 # Create a confusion matrix
-confusion_matrix <- confusionMatrix(bank_clean1.predictions, bank_clean1.test$y)
+confusion_matrix <- confusionMatrix(bank_clean.predictions, bank_clean.test$y)
 print(confusion_matrix)
 
 # Calculate accuracy, precision, and recall
-cm <- confusionMatrix(bank_clean1.predictions, bank_clean1.test$y)
+cm <- confusionMatrix(bank_clean.predictions, bank_clean.test$y)
 accuracy <- cm$overall["Accuracy"]
 print(accuracy)
 precision <- cm$byClass["Pos Pred Value"]
 print(precision)
 recall <- cm$byClass["Sensitivity"]
 print(recall)
+
